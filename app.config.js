51cfd30f668152e5f0faaef0bb65ff7d3d8c4932
Quentin <qhonnart@amphibee.fr>
2024-02-28 23:12:01 +0100

    update

--- app.config.js
+++ app.config.js
@@ -2,6 +2,7 @@ module.exports = {
   expo: {
     name: "spacecraft",
     slug: "spacecraft",
+    owner: "quentin_honnart",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
@@ -19,13 +20,17 @@ module.exports = {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
-      }
+      },
+      "package": "com.spacecraft"
     },
     web: {
       favicon: "./assets/favicon.png"
     },
     extra: {
-      storybookEnabled: process.env.STORYBOOK_ENABLED
+      storybookEnabled: process.env.STORYBOOK_ENABLED,
+      eas: {
+        projectId: "7650ff8c-15d6-416f-b5af-8ba7d99ec125"
+      }
     }
   }
 };
